874bf0f981b510396cef82fb7739f07a3a62fe2d
deansyue <c1318131801@gmail.com>
2022-01-05 22:23:23 +0800

    feat: add users router

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,19 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 // 與mongodb資料庫連線
 require('../config/mongoose')
 
-// 將網址結構符合 / 字串開頭的 request 導向 home 模組 
-router.use('/', home)
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組
 router.use('/restaurants', restaurants)
+// 將網址結構符合 /users 字串開頭的 request 導向 users 模組
+router.use('/users', users)
 // 將網址結構符合 /search 字串開頭的 request 導向 search 模組
 router.use('/search', search)
+// 將網址結構符合 / 字串開頭的 request 導向 home 模組 
+router.use('/', home)
 
 // 匯出路由模組
 module.exports = router
